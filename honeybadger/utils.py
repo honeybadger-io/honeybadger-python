435d3c604a195aad106a2a3b88822fc1d23b45d1
kelvin <kelvinadigwu@yahoo.com>
2024-04-06 08:55:37 +0100

    Iterate over data_copy

--- honeybadger/utils.py
+++ honeybadger/utils.py
@@ -17,18 +17,18 @@ def filter_dict(data, filter_keys):
 
     data_copy = copy.deepcopy(data)
 
-    for key, value in data.items():
+    for key, value in data_copy.items():
         # While tuples are considered valid dictionary keys,
         # they are not json serializable
         # so we remove them from the dictionary
         if type(key) == tuple:
-            data_copy.pop(key)
+            data.pop(key)
             continue
 
         if key in filter_keys:
-            data_copy[key] = "[FILTERED]"
+            data[key] = "[FILTERED]"
 
         if type(value) == dict:
-            data_copy[key] = filter_dict(data[key], filter_keys)
+            data[key] = filter_dict(data[key], filter_keys)
 
-    return data_copy
+    return data
