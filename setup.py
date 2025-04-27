2c44b6a70f6968a346a613b813f565a964ffaf60
Kevin Webster <kevin@kevinwebster.me>
2025-04-27 09:06:03 -0700

    Changes from black

--- setup.py
+++ setup.py
@@ -6,31 +6,28 @@ from setuptools import setup
 
 
 def get_version():
-    with open('honeybadger/version.py', encoding='utf-8') as f:
+    with open("honeybadger/version.py", encoding="utf-8") as f:
         return re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', f.read(), re.M).group(1)
 
 
 setup(
-    name='honeybadger',
+    name="honeybadger",
     version=get_version(),
-    description='Send Python and Django errors to Honeybadger',
-    url='https://github.com/honeybadger-io/honeybadger-python',
-    author='Dave Sullivan',
-    author_email='dave@davesullivan.ca',
-    license='MIT',
-    packages=['honeybadger', 'honeybadger.contrib'],
+    description="Send Python and Django errors to Honeybadger",
+    url="https://github.com/honeybadger-io/honeybadger-python",
+    author="Dave Sullivan",
+    author_email="dave@davesullivan.ca",
+    license="MIT",
+    packages=["honeybadger", "honeybadger.contrib"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: System :: Monitoring'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: System :: Monitoring",
     ],
-    install_requires=[
-        'psutil',
-        'six'
-    ]
+    install_requires=["psutil", "six"],
 )
