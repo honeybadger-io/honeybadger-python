1643f8e83f2b8c33899e4e742032892192414276
Kevin Webster <kevin@kevinwebster.me>
2025-05-06 08:41:44 -0700

    Run black again

--- honeybadger/__init__.py
+++ honeybadger/__init__.py
@@ -18,10 +18,13 @@ honeybadger = Honeybadger()
 honeybadger.wrap_excepthook(sys.excepthook)
 
 orig = signal.getsignal(signal.SIGTERM)
+
+
 def _on_term(signum, frame):
     if callable(orig):
         orig(signum, frame)
     else:
         sys.exit(0)
+
 
 signal.signal(signal.SIGTERM, _on_term)
