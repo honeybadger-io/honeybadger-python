a8b0434cc7068839ac4a67c0c7eccf6b54f29edd
Roel Bondoc <rsbondoc@gmail.com>
2025-05-08 10:26:26 -0400

    Format

--- honeybadger/tests/test_notice.py
+++ honeybadger/tests/test_notice.py
@@ -3,6 +3,7 @@ import pytest
 from honeybadger.config import Configuration
 from honeybadger.notice import Notice
 
+
 def test_notice_initialization():
     # Test with exception
     exception = Exception("Test exception")
@@ -13,7 +14,10 @@ def test_notice_initialization():
 
     # Test with error_class and error_message
     notice = Notice(error_class="TestError", error_message="Test message")
-    assert notice.exception == { "error_class": "TestError", "error_message": "Test message" }
+    assert notice.exception == {
+        "error_class": "TestError",
+        "error_message": "Test message",
+    }
     assert notice.error_class == "TestError"
     assert notice.error_message == "Test message"
 
@@ -27,28 +31,26 @@ def test_notice_initialization():
     assert notice.exception == exception
     assert notice.context["error_message"] == "Test message"
 
+
 def test_notice_excluded_exception():
-    config = Configuration(excluded_exceptions = ["TestError", "Exception"])
+    config = Configuration(excluded_exceptions=["TestError", "Exception"])
 
     # Test with excluded exception
     notice = Notice(
-        error_class="TestError",
-        error_message="Test message",
-        config=config
+        error_class="TestError", error_message="Test message", config=config
     )
     assert notice.excluded_exception() is True
 
     # Test with non-excluded exception
     notice = Notice(
-        error_class="NonExcludedError",
-        error_message="Test message",
-        config=config
+        error_class="NonExcludedError", error_message="Test message", config=config
     )
     assert notice.excluded_exception() is False
 
     # Test with exception
     notice = Notice(exception=Exception("Test exception"), config=config)
     assert notice.excluded_exception() is True
+
 
 def test_notice_payload():
     config = Configuration()
@@ -60,7 +62,9 @@ def test_notice_payload():
     assert payload["error"]["message"] == "Test exception"
 
     # Test with error_class and error_message
-    notice = Notice(error_class="TestError", error_message="Test message", config=config)
+    notice = Notice(
+        error_class="TestError", error_message="Test message", config=config
+    )
     payload = notice.payload
     assert payload["error"]["class"] == "TestError"
     assert payload["error"]["message"] == "Test message"
